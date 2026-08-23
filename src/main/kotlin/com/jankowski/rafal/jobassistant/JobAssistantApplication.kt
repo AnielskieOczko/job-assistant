ad0556bd2271ed441384605ad7abf52bdbf978dd
Rafal Jankowski <rafaljankowski7@gmail.com>
2026-08-23 11:42:41 +0200

    Init commit

---
+++ src/main/kotlin/com/jankowski/rafal/jobassistant/JobAssistantApplication.kt
@@ -0,0 +1,11 @@
+package com.jankowski.rafal.jobassistant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobAssistantApplication
+
+fun main(args: Array<String>) {
+	runApplication<JobAssistantApplication>(*args)
+}
---
+++ src/test/kotlin/com/jankowski/rafal/jobassistant/JobAssistantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jankowski.rafal.jobassistant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobAssistantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
